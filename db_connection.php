bfdba6525d7ddd99b7b859db856e3084755dc015
Сергей <56017574+SERgey1312@users.noreply.github.com>
2021-07-03 21:36:30 +0300

    add db_connection

--- db_connection.php
+++ db_connection.php
@@ -1,1 +1,11 @@
 <?php
+$url = parse_url(getenv("DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+
+$conn->query('show tables;');
