f7cd020d5d824a24a2af66ae761593609b2f6410
Сергей <56017574+SERgey1312@users.noreply.github.com>
2021-07-04 11:20:17 +0300

    test

--- from_db.php
+++ from_db.php
@@ -1,24 +1,25 @@
 <?php
-require_once ('db_connection.php');
+//require_once ('db_connection.php');
+//
+//$sql_from_db = "select * from comments";
+//$comments = $conn->query($sql_from_db);
+//$commentsArr = [];
+//foreach($comments as $row){
+//    $commentId = $row["id"];
+//    $commentName = $row["name"];
+//    $commentEmail = $row["email"];
+//    $commentBody = $row["comment"];
+//
+//    $arr = [
+//        "id" => $commentId,
+//        "name" => $commentName,
+//        "email" => $commentEmail,
+//        "comment" => $commentBody,
+//    ];
+//
+//    array_push($commentsArr, $arr);
+//}
+//
+//echo json_encode($commentsArr);
 
-$sql_from_db = "select * from comments";
-$comments = $conn->query($sql_from_db);
-$commentsArr = [];
-foreach($comments as $row){
-    $commentId = $row["id"];
-    $commentName = $row["name"];
-    $commentEmail = $row["email"];
-    $commentBody = $row["comment"];
-
-    $arr = [
-        "id" => $commentId,
-        "name" => $commentName,
-        "email" => $commentEmail,
-        "comment" => $commentBody,
-    ];
-
-    array_push($commentsArr, $arr);
-}
-
-echo json_encode($commentsArr);
-
+echo 1;
